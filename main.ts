ed85b62b3085f32f111841aea5dcc3da19717c24
The Senate <66320474+Judge7567@users.noreply.github.com>
2022-07-14 14:46:49 -0700

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -3,40 +3,31 @@ namespace SpriteKind {
 }
 let projectile2: Sprite = null
 let projectile: Sprite = null
-let bULLET: Sprite = null
 let HEALTH = 3
 let sNAKE = sprites.create(assets.image`SS`, SpriteKind.Player)
 let gURAD = sprites.create(assets.image`GURD`, SpriteKind.Enemy)
+let bULLET = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 1 1 . . . . . . . 
+    . . . . . . . 1 1 . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Projectile)
 tiles.setCurrentTilemap(tilemap`level1`)
 scene.cameraFollowSprite(sNAKE)
 tiles.placeOnTile(sNAKE, tiles.getTileLocation(1, 38))
 tiles.placeOnTile(gURAD, tiles.getTileLocation(4, 30))
-forever(function () {
-    bULLET = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . 1 1 . . . . . . . 
-        . . . . . . . 1 1 . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, SpriteKind.Projectile)
-    if (sNAKE.overlapsWith(bULLET)) {
-        HEALTH += -1
-    }
-    if (HEALTH == 0) {
-        game.over(false)
-    }
-})
 forever(function () {
     sNAKE.vy = 70
     if (controller.up.isPressed() && (sNAKE.tileKindAt(TileDirection.Bottom, assets.tile`myTile`) || (sNAKE.tileKindAt(TileDirection.Bottom, assets.tile`myTi`) || false))) {
@@ -89,8 +80,30 @@ forever(function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             `, sNAKE, -150, 0)
-        pause(200)
     } else {
         sNAKE.vx = 0
+    }
+})
+forever(function () {
+    if (sNAKE.overlapsWith(bULLET)) {
+        console.log("HIT!")
+        HEALTH += -1
+        pause(500)
+    }
+    if (HEALTH == 0) {
+        game.over(false, effects.dissolve)
+    }
+})
+forever(function () {
+    if (projectile2.overlapsWith(gURAD) || projectile.overlapsWith(gURAD)) {
+        let mySprite: Sprite = null
+        mySprite.destroy()
+    }
+})
+forever(function () {
+    if (true) {
+        bULLET.setPosition(gURAD.x, gURAD.y)
+        pause(500)
+        bULLET.setVelocity(50, 0)
     }
 })
