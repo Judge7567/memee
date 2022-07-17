01c462ee605e486f3cfb0efcbe7cf007821859c6
The Senate <66320474+Judge7567@users.noreply.github.com>
2022-07-16 18:39:03 -0700

    Update pxt.json, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -8,6 +8,12 @@ function GURDshoot () {
         bULLET.setVelocity(150, 0)
     }
 }
+function Ded1 () {
+    if (projectile2.overlapsWith(gURAD)) {
+        bULLET.destroy()
+        gURAD.destroy(effects.spray, 500)
+    }
+}
 function Shoot () {
     if (sNAKE.image.equals(assets.image`SS`)) {
         projectile = sprites.createProjectileFromSprite(assets.image`Project`, sNAKE, 150, 0)
@@ -18,20 +24,38 @@ function Shoot () {
         pause(500)
     }
 }
+function GURDshoot2 () {
+    if (true) {
+        bullet2.setPosition(sniper.x, sniper.y)
+        pause(1000)
+        bullet2.setVelocity(-500, 0)
+    }
+}
+function ded2 () {
+    if (projectile.overlapsWith(sniper)) {
+        bullet2.destroy()
+        sniper.destroy(effects.spray, 500)
+    }
+}
+let list: Sprite[] = []
 let projectile2: Sprite = null
 let projectile: Sprite = null
 let bULLET: Sprite = null
+let bullet2: Sprite = null
+let sniper: Sprite = null
 let gURAD: Sprite = null
 let sNAKE: Sprite = null
-let HEALTH = 3
+let HEALTH = 7
 sNAKE = sprites.create(assets.image`SS`, SpriteKind.Player)
 gURAD = sprites.create(assets.image`GURD`, SpriteKind.Enemy)
-let bullet2 = sprites.create(assets.image`Project`, SpriteKind.Player)
+sniper = sprites.create(assets.image`sniper`, SpriteKind.Enemy)
+bullet2 = sprites.create(assets.image`Project`, SpriteKind.Projectile)
 bULLET = sprites.create(assets.image`Project`, SpriteKind.Projectile)
 projectile = sprites.create(assets.image`Project`, SpriteKind.Projectile)
 projectile2 = sprites.create(assets.image`Project`, SpriteKind.Projectile)
 tiles.setCurrentTilemap(tilemap`level1`)
 scene.cameraFollowSprite(sNAKE)
+tiles.placeOnTile(sniper, tiles.getTileLocation(13, 27))
 tiles.placeOnTile(sNAKE, tiles.getTileLocation(1, 38))
 tiles.placeOnTile(gURAD, tiles.getTileLocation(4, 30))
 forever(function () {
@@ -71,21 +95,24 @@ forever(function () {
     }
 })
 forever(function () {
+    Ded1()
+    ded2()
     GURDshoot()
+    GURDshoot2()
 })
 forever(function () {
-    if (sNAKE.overlapsWith(bULLET)) {
+    if (sNAKE.overlapsWith(bULLET) || sNAKE.overlapsWith(bullet2)) {
         console.log("HIT!")
         HEALTH += -1
         pause(500)
     }
     if (HEALTH <= 0) {
-        game.over(false, effects.confetti)
+        game.over(false, effects.dissolve)
     }
 })
 forever(function () {
-    if (projectile2.overlapsWith(gURAD)) {
-        bULLET.destroy()
-        gURAD.destroy(effects.spray, 500)
+    list = [gURAD]
+    for (let value of list) {
+    	
     }
 })
